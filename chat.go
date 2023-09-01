2da82f1d262b82c6e966359fa00a6db0f251682f
YohanesFS <yohanesfelix@hotmail.com>
2023-09-01 21:03:40 +0800

    Changed the `SendMessage` and `ReceiveMessage` functions in `chat.go`.

--- chat.go
+++ chat.go
@@ -17,9 +17,9 @@ func NewChat(id int, message string, from User, to User) *Chat {
 }
 
 func (c *Chat) SendMessage() {
-	fmt.Printf("%s sent a message: %s\n", c.From.Name, c.Message)
+	fmt.Printf("%s sent a message: %s\n", c.From.Username, c.Message)
 }
 
 func (c *Chat) ReceiveMessage() {
-	fmt.Printf("%s received a message: %s\n", c.To.Name, c.Message)
+	fmt.Printf("%s received a message: %s\n", c.To.Username, c.Message)
 }
