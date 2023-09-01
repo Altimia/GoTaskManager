ea4484dfc0ccd72d4624ed7c5e48fe2c8b66cbfc
YohanesFS <yohanesfelix@hotmail.com>
2023-09-01 21:01:28 +0800

    Implemented InitAPI function to initialize the Gin engine and set up a GET route for /ping.

--- api.go
+++ api.go
@@ -4,10 +4,26 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+var router *gin.Engine
+
 func InitAPI() {
-	// code for initializing the API
+	router = gin.Default()
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	router.Run() // listen and serve on 0.0.0.0:8080
 }
 
 func CloseAPI() {
 	// code for closing the API
+	// This function will be implemented later when we have a way to gracefully shutdown the server
 }
