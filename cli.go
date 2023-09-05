35047b06413dcc0c7c2eba288099ae8c55e9d1fa
YohanesFS <yohanesfelix@hotmail.com>
2023-09-05 20:23:39 +0800

    Implemented actual implementation of the subcommands and handlers.

--- cli.go
+++ cli.go
@@ -23,6 +23,47 @@ var taskCmd = &cobra.Command{
 	Long:  `Add, view, update, and delete tasks.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		// code for running the task command
+		// This will be implemented in the next steps
+	},
+}
+
+var addTaskCmd = &cobra.Command{
+	Use:   "add",
+	Short: "Add a task",
+	Long:  `Add a new task to the task list.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// code for adding a task
+		// This will be implemented in the next steps
+	},
+}
+
+var viewTaskCmd = &cobra.Command{
+	Use:   "view",
+	Short: "View a task",
+	Long:  `View the details of a task.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// code for viewing a task
+		// This will be implemented in the next steps
+	},
+}
+
+var updateTaskCmd = &cobra.Command{
+	Use:   "update",
+	Short: "Update a task",
+	Long:  `Update the details of a task.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// code for updating a task
+		// This will be implemented in the next steps
+	},
+}
+
+var deleteTaskCmd = &cobra.Command{
+	Use:   "delete",
+	Short: "Delete a task",
+	Long:  `Delete a task from the task list.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// code for deleting a task
+		// This will be implemented in the next steps
 	},
 }
 
@@ -34,6 +75,37 @@ var userCmd = &cobra.Command{
 	Long:  `Register, login, and manage user profiles.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		// code for running the user command
+		// This will be implemented in the next steps
+	},
+}
+
+var registerCmd = &cobra.Command{
+	Use:   "register",
+	Short: "Register a user",
+	Long:  `Register a new user.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// code for registering a user
+		// This will be implemented in the next steps
+	},
+}
+
+var loginCmd = &cobra.Command{
+	Use:   "login",
+	Short: "Login a user",
+	Long:  `Login a user.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// code for logging in a user
+		// This will be implemented in the next steps
+	},
+}
+
+var manageProfileCmd = &cobra.Command{
+	Use:   "manage",
+	Short: "Manage user profile",
+	Long:  `Manage the profile of a user.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// code for managing a user profile
+		// This will be implemented in the next steps
 	},
 }
 
@@ -45,6 +117,27 @@ var chatCmd = &cobra.Command{
 	Long:  `Send and receive chat messages.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		// code for running the chat command
+		// This will be implemented in the next steps
+	},
+}
+
+var sendMessageCmd = &cobra.Command{
+	Use:   "send",
+	Short: "Send a message",
+	Long:  `Send a chat message to a user.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// code for sending a chat message
+		// This will be implemented in the next steps
+	},
+}
+
+var receiveMessageCmd = &cobra.Command{
+	Use:   "receive",
+	Short: "Receive a message",
+	Long:  `Receive a chat message from a user.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// code for receiving a chat message
+		// This will be implemented in the next steps
 	},
 }
 
