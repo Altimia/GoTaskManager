93e76eabaa75b19f511d4a7362bef43acd306f02
YohanesFS <yohanesfelix@hotmail.com>
2023-09-05 20:19:53 +0800

    Complete CLI file: add commands for task and user operations, chat functionality.

--- cli.go
+++ cli.go
@@ -8,11 +8,29 @@ var rootCmd = &cobra.Command{
 	Use:   "gotask",
 	Short: "GoTask is a command-line task manager",
 	Long:  `A command-line task manager where users can add, view, update, delete tasks, and communicate with other users in real-time.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		// code for running the command
-	},
 }
 
 func Execute() {
-	// code for executing the command
+	rootCmd.AddCommand(taskCmd)
+	rootCmd.AddCommand(userCmd)
+	rootCmd.AddCommand(chatCmd)
+	rootCmd.Execute()
+}
+
+var taskCmd = &cobra.Command{
+	Use:   "task",
+	Short: "Manage tasks",
+	Long:  `Add, view, update, and delete tasks.`,
+}
+
+var userCmd = &cobra.Command{
+	Use:   "user",
+	Short: "Manage users",
+	Long:  `Register, login, and manage user profiles.`,
+}
+
+var chatCmd = &cobra.Command{
+	Use:   "chat",
+	Short: "Chat with users",
+	Long:  `Send and receive chat messages.`,
 }
