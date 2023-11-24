136e52fe9e2b50dc10476c8affa12e640d825e45
YohanesFS <yohanesfelix@hotmail.com>
2023-11-24 22:45:29 +0800

    Fixed issue with `router.Shutdown` being undefined for `*gin.Engine` by modifying the `api.go` file to correctly shut down the Gin router using an `http.Server` instance.

--- api.go
+++ api.go
@@ -20,13 +20,42 @@ func InitAPI() {
 		})
 	})
 
-	router.Run() // listen and serve on 0.0.0.0:8080
+	// Define the server with a specific address and attach the router
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	// Run the server in a goroutine so that it doesn't block
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			fmt.Printf("ListenAndServe: %v\n", err)
+		}
+	}()
+}
+
+// Define a global server variable to be used for graceful shutdown
+var server *http.Server
+
+func InitAPI() {
+	router = gin.Default()
+
+	// ... (other code remains unchanged)
+
+	// Initialize the server with the router
+	server = &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	// ... (other code remains unchanged)
 }
 
 func CloseAPI() {
+	// Use the global server variable for shutdown
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-	if err := router.Shutdown(ctx); err != nil {
+	if err := server.Shutdown(ctx); err != nil {
 		fmt.Printf("Error shutting down the server: %v\n", err)
 	} else {
 		fmt.Println("Server shut down gracefully")
