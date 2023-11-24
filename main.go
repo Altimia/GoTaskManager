2372a1bd7627b9f09bdb6a8c4d77cfbff3ed5b2b
YohanesFS <yohanesfelix@hotmail.com>
2023-11-24 22:58:23 +0800

    Switched to using Zap for logging in the Go codebase.

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"context"
-	"log"
+	"go.uber.org/zap"
 	"net/http"
 	"os"
 	"os/signal"
@@ -25,40 +25,43 @@ func handleConnections(w http.ResponseWriter, r *http.Request) {
 	// Upgrade initial GET request to a websocket
 	ws, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		log.Printf("Error upgrading GET request to websocket: %v", err)
+		zap.L().Error("Error upgrading GET request to websocket", zap.Error(err))
 		http.Error(w, "Could not open websocket connection", http.StatusBadRequest)
 		return
 	}
 	defer func() {
 		if err := ws.Close(); err != nil {
-			log.Printf("Error closing websocket: %v", err)
+			zap.L().Error("Error closing websocket", zap.Error(err))
 		}
 	}() // Make sure we close the connection when the function returns
 
 	// Register the connection with the user's ID
 	userID, err := strconv.Atoi(r.URL.Query().Get("user_id"))
 	if err != nil {
-		log.Printf("Error converting user_id to int: %v", err)
+		zap.L().Error("Error converting user_id to int", zap.Error(err))
 		return
 	}
 	userConnections[uint(userID)] = ws
-	log.Printf("User with id %d connected via websocket", userID)
+	zap.L().Info("User connected via websocket", zap.Int("userID", userID))
 
 	// Infinite loop to continuously read incoming messages
 	for {
 		_, msg, err := ws.ReadMessage()
 		if err != nil {
-			log.Printf("Error reading message: %v", err)
+			zap.L().Error("Error reading message", zap.Error(err))
 			delete(userConnections, uint(userID)) // Remove the connection if there's an error
-			log.Printf("User with id %d disconnected", userID)
+			zap.L().Info("User disconnected", zap.Int("userID", userID))
 			return
 		}
 		// Log the message
-		log.Printf("Message received from user with id %d: %s", userID, msg)
+		zap.L().Info("Message received from user", zap.Int("userID", userID), zap.ByteString("message", msg))
 	}
 }
 
 func main() {
+	logger, _ := zap.NewProduction()
+	defer logger.Sync() // flushes buffer, if any
+	zap.ReplaceGlobals(logger)
 	// Configure websocket route
 	http.HandleFunc("/ws", handleConnections)
 
@@ -70,14 +73,14 @@ func main() {
 	server := &http.Server{Addr: ":8000", Handler: nil}
 	go func() {
 		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatalf("Error starting server: %v", err)
+			zap.L().Fatal("Error starting server", zap.Error(err))
 		}
 	}()
-	log.Println("Server started on :8000")
+	zap.L().Info("Server started", zap.String("address", ":8000"))
 
 	// Block until a signal is received
 	<-signalChan
-	log.Println("Shutting down server...")
+	zap.L().Info("Shutting down server...")
 
 	// Call CloseAPI to handle graceful shutdown of the API server
 	CloseAPI()
@@ -87,7 +90,7 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	if err := server.Shutdown(ctx); err != nil {
-		log.Fatalf("Server shutdown failed: %+v", err)
+		zap.L().Fatal("Server shutdown failed", zap.Error(err))
 	}
-	log.Println("Server exited properly")
+	zap.L().Info("Server exited properly")
 }
--- task.go
+++ task.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"go.uber.org/zap"
 
 	"github.com/gorilla/websocket"
 	"github.com/jinzhu/gorm"
@@ -18,31 +18,31 @@ type Task struct {
 
 func AddTask(task Task) error {
 	if err := db.Create(&task).Error; err != nil {
-		log.Printf("Error adding task: %v", err)
+		zap.L().Error("Error adding task", zap.Error(err))
 		return err
 	}
-	log.Println("Task added successfully")
+	zap.L().Info("Task added successfully")
 	return nil
 }
 
 func ViewTask(id int) (Task, error) {
 	var task Task
 	if err := db.First(&task, id).Error; err != nil {
-		log.Printf("Error viewing task with id %d: %v", id, err)
+		zap.L().Error("Error viewing task", zap.Int("id", id), zap.Error(err))
 		return Task{}, err
 	}
-	log.Printf("Task with id %d viewed successfully", id)
+	zap.L().Info("Task viewed successfully", zap.Int("id", id))
 	return task, nil
 }
 
 func UpdateTask(id int, updatedTask Task) error {
 	var task Task
 	if err := db.First(&task, id).Error; err != nil {
-		log.Printf("Error finding task with id %d for update: %v", id, err)
+		zap.L().Error("Error finding task for update", zap.Int("id", id), zap.Error(err))
 		return err
 	}
 	if err := db.Model(&task).Updates(updatedTask).Error; err != nil {
-		log.Printf("Error updating task with id %d: %v", id, err)
+		zap.L().Error("Error updating task", zap.Int("id", id), zap.Error(err))
 		return err
 	}
 	// Send notification to the assigned user if they are connected
@@ -50,19 +50,19 @@ func UpdateTask(id int, updatedTask Task) error {
 		notification := fmt.Sprintf("Task '%s' has been updated.", task.Name)
 		conn.WriteMessage(websocket.TextMessage, []byte(notification))
 	}
-	fmt.Println("Task updated successfully")
+	zap.L().Info("Task updated successfully", zap.Int("id", id))
 	return nil
 }
 
 func DeleteTask(id int) {
 	var task Task
 	if err := db.First(&task, id).Error; err != nil {
-		log.Printf("Error finding task with id %d for deletion: %v", id, err)
+		zap.L().Error("Error finding task for deletion", zap.Int("id", id), zap.Error(err))
 		return
 	}
 	if err := db.Delete(&task).Error; err != nil {
-		log.Printf("Error deleting task with id %d: %v", id, err)
+		zap.L().Error("Error deleting task", zap.Int("id", id), zap.Error(err))
 		return
 	}
-	log.Printf("Task with id %d deleted successfully", id)
+	zap.L().Info("Task deleted successfully", zap.Int("id", id))
 }
