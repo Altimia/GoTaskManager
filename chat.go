cfc51027c274bf333f7e6510e92205b0e5e41e8b
YohanesFS <yohanesfelix@hotmail.com>
2023-11-24 17:30:35 +0800

    Implemented error handling for the chat system.

--- chat.go
+++ chat.go
@@ -21,17 +21,21 @@ func NewChat(id int, message string, from User, to User) *Chat {
 	}
 }
 
-func (c *Chat) SendMessage(message string) {
+func (c *Chat) SendMessage(message string) error {
 	c.Messages = append(c.Messages, message)
-	c.Conn.WriteMessage(websocket.TextMessage, []byte(message))
+	err := c.Conn.WriteMessage(websocket.TextMessage, []byte(message))
+	if err != nil {
+		return fmt.Errorf("error sending message: %w", err)
+	}
 	fmt.Printf("%s sent a message: %s\n", c.From.Username, message)
+	return nil
 }
 
-func (c *Chat) ReceiveMessage() {
+func (c *Chat) ReceiveMessage() error {
 	for {
 		_, message, err := c.Conn.ReadMessage()
 		if err != nil {
-			return
+			return fmt.Errorf("error receiving message: %w", err)
 		}
 		c.Messages = append(c.Messages, string(message))
 		fmt.Printf("%s received a message: %s\n", c.To.Username, string(message))
--- main.go
+++ main.go
@@ -17,18 +17,25 @@ func handleConnections(w http.ResponseWriter, r *http.Request) {
 	// Upgrade initial GET request to a websocket
 	ws, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
+		log.Printf("error upgrading GET request to websocket: %v", err)
+		http.Error(w, "Could not open websocket connection", http.StatusBadRequest)
 		return
 	}
 	defer ws.Close() // Make sure we close the connection when the function returns
 
 	// Register the connection with the user's ID
-	userID, _ := strconv.Atoi(r.URL.Query().Get("user_id"))
+	userID, err := strconv.Atoi(r.URL.Query().Get("user_id"))
+	if err != nil {
+		log.Printf("error converting user_id to int: %v", err)
+		return
+	}
 	userConnections[uint(userID)] = ws
 
 	// Infinite loop to continuously read incoming messages
 	for {
 		_, msg, err := ws.ReadMessage()
 		if err != nil {
+			log.Printf("error reading message: %v", err)
 			delete(userConnections, uint(userID)) // Remove the connection if there's an error
 			return
 		}
