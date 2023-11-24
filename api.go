39a604f6d770654f0e31bd94f248705d0dded247
YohanesFS <yohanesfelix@hotmail.com>
2023-11-24 22:37:25 +0800

    Implemented CloseAPI function to gracefully shutdown the server.

--- api.go
+++ api.go
@@ -20,7 +20,19 @@ func InitAPI() {
 	router.Run() // listen and serve on 0.0.0.0:8080
 }
 
+import (
+	"context"
+	"time"
+)
+
+// ...
+
 func CloseAPI() {
-	// code for closing the API
-	// This function will be implemented later when we have a way to gracefully shutdown the server
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := router.Shutdown(ctx); err != nil {
+		fmt.Printf("Error shutting down the server: %v\n", err)
+	} else {
+		fmt.Println("Server shut down gracefully")
+	}
 }
