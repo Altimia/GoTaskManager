2f1d414fd470358fc3595a151fa2df9b8b5f4f22
YohanesFS <yohanesfelix@hotmail.com>
2023-11-27 13:54:52 +0800

    Refactor chat.go to handle receiving messages with a stop channel.

--- chat.go
+++ chat.go
@@ -26,23 +26,34 @@ func NewChat(id int, from User, to User, conn *websocket.Conn) *Chat {
 }
 
 func (c *Chat) SendMessage(message string) error {
+	if c.Conn == nil {
+		return fmt.Errorf("no websocket connection")
+	}
 	c.Messages = append(c.Messages, message)
 	if err := c.Conn.WriteMessage(websocket.TextMessage, []byte(message)); err != nil {
 		zap.L().Error("Error sending message", zap.Error(err))
-		return fmt.Errorf("Error sending message: %w", err)
+		return fmt.Errorf("error sending message: %w", err)
 	}
 	zap.L().Info("Message sent", zap.String("from", c.From.Username), zap.String("message", message))
 	return nil
 }
 
-func (c *Chat) ReceiveMessage() error {
+func (c *Chat) ReceiveMessage(stopChan <-chan struct{}) error {
+	if c.Conn == nil {
+		return fmt.Errorf("no websocket connection")
+	}
 	for {
-		_, message, err := c.Conn.ReadMessage()
-		if err != nil {
-			zap.L().Error("Error receiving message", zap.Error(err))
-			return fmt.Errorf("Error receiving message: %w", err)
+		select {
+		case <-stopChan:
+			return nil
+		default:
+			_, message, err := c.Conn.ReadMessage()
+			if err != nil {
+				zap.L().Error("Error receiving message", zap.Error(err))
+				return fmt.Errorf("error receiving message: %w", err)
+			}
+			c.Messages = append(c.Messages, string(message))
+			zap.L().Info("Message received", zap.String("to", c.To.Username), zap.String("message", string(message)))
 		}
-		c.Messages = append(c.Messages, string(message))
-		zap.L().Info("Message received", zap.String("to", c.To.Username), zap.String("message", string(message)))
 	}
 }
