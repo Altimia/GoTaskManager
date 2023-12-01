3f51a62ac6a62c94d9e060d9c8347439e7979c3f
YohanesFS <yohanesfelix@hotmail.com>
2023-12-01 11:24:44 +0800

    Refactor test environment

--- user_test.go
+++ user_test.go
@@ -9,9 +9,54 @@ import (
 )
 
 func TestRegister(t *testing.T) {
-	// Existing TestRegister code...
+	// Mock database and expectations
+	db, mock, err := sqlmock.New()
+	assert.NoError(t, err)
+	defer db.Close()
+
+	// Set up the mock expectations
+	mock.ExpectBegin()
+	mock.ExpectExec("INSERT INTO `users`").WithArgs("testuser", "testpass", "testprofile").WillReturnResult(sqlmock.NewResult(1, 1))
+	mock.ExpectCommit()
+
+	// Use the mocked DB connection
+	gormDB, err := gorm.Open("sqlite3", "test.db")
+	if err != nil {
+		t.Fatalf("Failed to open gorm db: %v", err)
+	}
+	defer func() { gormDB.Close() }()
+
+	// Test the Register function
+	Register(User{Username: "testuser", Password: "testpass", Profile: "testprofile"})
+
+	// Ensure all expectations are met
+	assert.NoError(t, mock.ExpectationsWereMet())
 }
 
 func TestLogin(t *testing.T) {
-	// Existing TestLogin code...
+	// Mock database and expectations
+	db, mock, err := sqlmock.New()
+	assert.NoError(t, err)
+	defer db.Close()
+
+	// Set up the mock expectations
+	rows := sqlmock.NewRows([]string{"id", "username", "password", "profile"}).
+		AddRow(1, "testuser", "testpass", "testprofile")
+	mock.ExpectQuery("SELECT * FROM `users`").WithArgs("testuser", "testpass").WillReturnRows(rows)
+
+	// Use the mocked DB connection
+	gormDB, err := gorm.Open("sqlite3", "test.db")
+	if err != nil {
+		t.Fatalf("Failed to open gorm db: %v", err)
+	}
+	defer func() { gormDB.Close() }()
+
+	// Test the Login function
+	result := Login("testuser", "testpass")
+
+	// Check the result
+	assert.True(t, result)
+
+	// Ensure all expectations are met
+	assert.NoError(t, mock.ExpectationsWereMet())
 }
