f57014978bd8c4f9fd6e7baef767ae0624e712f6
YohanesFS <yohanesfelix@hotmail.com>
2023-12-01 21:50:08 +0800

    Implemented tests for the chat functionality in `chat_test.go`.

--- chat_test.go
+++ chat_test.go
@@ -1,3 +1,71 @@
 package main
 
-// Add real-time communication tests here
+import (
+	"errors"
+	"testing"
+	"github.com/gorilla/websocket"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+// MockWebSocketConn is a mock for the websocket.Conn
+type MockWebSocketConn struct {
+	mock.Mock
+}
+
+func (m *MockWebSocketConn) WriteMessage(messageType int, data []byte) error {
+	args := m.Called(messageType, data)
+	return args.Error(0)
+}
+
+func (m *MockWebSocketConn) ReadMessage() (messageType int, p []byte, err error) {
+	args := m.Called()
+	return args.Int(0), args.Get(1).([]byte), args.Error(2)
+}
+
+func TestSendMessage(t *testing.T) {
+	mockConn := new(MockWebSocketConn)
+	mockConn.On("WriteMessage", websocket.TextMessage, []byte("test message")).Return(nil)
+
+	chat := NewChat(1, User{Username: "fromUser"}, User{Username: "toUser"}, mockConn)
+	err := chat.SendMessage("test message")
+
+	assert.NoError(t, err)
+	assert.Equal(t, []string{"test message"}, chat.Messages)
+	mockConn.AssertExpectations(t)
+}
+
+func TestSendMessageNoConnection(t *testing.T) {
+	chat := NewChat(1, User{Username: "fromUser"}, User{Username: "toUser"}, nil)
+	err := chat.SendMessage("test message")
+
+	assert.Error(t, err)
+	assert.Equal(t, errors.New("no websocket connection"), err)
+}
+
+func TestReceiveMessage(t *testing.T) {
+	mockConn := new(MockWebSocketConn)
+	mockConn.On("ReadMessage").Return(websocket.TextMessage, []byte("received message"), nil)
+
+	chat := NewChat(1, User{Username: "fromUser"}, User{Username: "toUser"}, mockConn)
+	stopChan := make(chan struct{})
+	go func() {
+		chat.ReceiveMessage(stopChan)
+		close(stopChan)
+	}()
+
+	// Simulate receiving a message
+	stopChan <- struct{}{}
+
+	assert.Equal(t, []string{"received message"}, chat.Messages)
+	mockConn.AssertExpectations(t)
+}
+
+func TestReceiveMessageNoConnection(t *testing.T) {
+	chat := NewChat(1, User{Username: "fromUser"}, User{Username: "toUser"}, nil)
+	stopChan := make(chan struct{})
+	err := chat.ReceiveMessage(stopChan)
+
+	assert.Error(t, err)
+	assert.Equal(t, errors.New("no websocket connection"), err)
+}
