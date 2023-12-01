74942c5e7dfd6a9f67a55a8b5687c0f1049e09bb
YohanesFS <yohanesfelix@hotmail.com>
2023-12-01 11:32:29 +0800

    Implemented tests for API endpoints in `api_test.go`.

--- api_test.go
+++ api_test.go
@@ -1,3 +1,39 @@
 package main
 
 // Add API endpoint tests here
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPingRoute(t *testing.T) {
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/ping", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, "{\"message\":\"pong\"}", w.Body.String())
+}
+
+func TestMainAPI(t *testing.T) {
+	// Initialize the API routes
+	InitAPI()
+
+	// Create a test server
+	ts := httptest.NewServer(router)
+	defer ts.Close()
+
+	// Define more tests for other endpoints here
+}
