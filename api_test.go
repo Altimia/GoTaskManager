40e597db3954642917aee1873e1f262d039ed8dc
YohanesFS <yohanesfelix@hotmail.com>
2023-12-01 21:47:11 +0800

    Implemented test for `GET /ping` endpoint in `api_test.go`.

---
+++ api_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPingRoute(t *testing.T) {
+	router := setupRouter() // Assuming there is a function setupRouter that initializes the routes
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/ping", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, "{\"message\":\"pong\"}", w.Body.String())
+}
+
+// setupRouter should be defined in api.go or another file where the router is set up.
+// If it's not already defined, we need to define it or adjust the test accordingly.
