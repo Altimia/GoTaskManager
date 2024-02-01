c76414dc215cb225334173eed2591e8ccd7cb942
YohanesFS <yohanesfelix@hotmail.com>
2024-02-01 12:38:03 +0800

    The expected SQL query in the `TestLogin` function has been updated to match the actual query generated by GORM.

--- user_test.go
+++ user_test.go
@@ -46,7 +46,7 @@ func TestLogin(t *testing.T) {
 	defer db.Close()
 
 	// Set up the mock expectations
-	expectedSQL := "SELECT \\* FROM \"users\" WHERE \\(username = \\? AND password = \\?\\)"
+	expectedSQL := "SELECT \\* FROM \"users\" WHERE \\(username = \\? AND password = \\?\\) ORDER BY \"users\".\"id\" ASC LIMIT 1"
 	currentTime := time.Now()
 	rows := sqlmock.NewRows([]string{"id", "username", "password", "profile", "created_at", "updated_at", "deleted_at"}).
 		AddRow(1, "testuser", "testpass", "testprofile", currentTime, currentTime, nil)
